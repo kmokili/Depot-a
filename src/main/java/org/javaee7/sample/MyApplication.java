3bde4a635795751db80b03501b7d6bea58a03240
Kevin Mokili <kevin.mokili@gmail.com>
2016-04-19 16:08:54 +0200

    Ajout Class to do dans MyApplication.java

--- src/main/java/org/javaee7/sample/MyApplication.java
+++ src/main/java/org/javaee7/sample/MyApplication.java
@@ -3,6 +3,8 @@ package org.javaee7.sample;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+/* Class to do */
+/* Class to do */
 @ApplicationPath("/resources")
 public class MyApplication extends Application {
 
