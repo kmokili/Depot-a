20532687040e834234abf591946c41eba7f1f98f
Kevin Mokili <kevin.mokili@gmail.com>
2016-04-19 16:40:02 +0200

    - Class to do

--- src/main/java/org/javaee7/sample/MyApplication.java
+++ src/main/java/org/javaee7/sample/MyApplication.java
@@ -4,7 +4,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 /* Class to do */
-/* Class to do */
+
 @ApplicationPath("/resources")
 public class MyApplication extends Application {
 
